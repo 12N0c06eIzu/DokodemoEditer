b4d70f73acc47c2da13a0f0bc78c4f84612206b9
Rikuto Hayashi <ritu6811@gmail.com>
2019-12-05 14:48:58 +0900

    add_uploadFile ファイルをアップロードできるようにした。

--- Ace.c9.io/js/dounload.js
+++
@@ -1,7 +0,0 @@
-function downloadFile(e){
-    var content = editor.getValue();
-    var blob = new Blob([content], {type: "text/html"});
-    e.target.href = window.URL.createObjectURL(blob);
-}
-document.getElementById('download').addEventListener('click', downloadFile, false);
-
