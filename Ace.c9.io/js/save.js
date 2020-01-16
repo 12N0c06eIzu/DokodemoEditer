330be5d8075db02d0e59ef0d78a4e84aa9454fd4
Rikuto Hayashi <ritu6811@gmail.com>
2020-01-16 16:05:43 +0900

    add_save sessionに保存することと呼び出しすることを追加

---
+++ Ace.c9.io/js/save.js
@@ -0,0 +1,25 @@
+var buildDom = require("ace/lib/dom").buildDom;
+
+var refs = {};
+function updateToolbar() {
+    refs.saveButton.disabled = editor.session.getUndoManager().isClean();
+    
+}
+editor.session.setValue(localStorage.savedValue || "Welcome to ace Toolbar demo!")
+    function txtSave() {
+        console.log("sdjjajfajl");
+        console.log(localStorage.savedValue);
+        localStorage.savedValue = editor.getValue(); 
+        editor.session.getUndoManager().markClean();
+        // updateToolbar();
+    }
+    function txtLoad(){
+        editor.setValue(localStorage.savedValue);
+    }
+
+    editor.commands.addCommand({
+        name: "save",
+        exec: txtSave,
+        bindKey: { win: "ctrl-s", mac: "cmd-s" }
+    });
+    
