37160207187e0b857cddd3a015e042133078d9af
Rikuto Hayashi <ritu6811@gmail.com>
2020-01-16 10:36:40 +0900

    fix buttonタグの中にaタグを入れていたがよくないらしいので buttonタグだけでダウンロードを実行できるようにした。

--- Ace.c9.io/js/filecomplete.js
+++ Ace.c9.io/js/filecomplete.js
@@ -1,9 +1,13 @@
 function downloadFile(e){
     var content = editor.getValue();
     var blob = new Blob([content], {type: "text/html"});
-    e.target.href = window.URL.createObjectURL(blob);
+    // e.target.href = window.URL.createObjectURL(blob);
+    let link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "index.html";
+    link.click();
 }
-document.getElementById('download').addEventListener('click', downloadFile, false);
+
 
 var element = document.getElementById("s_file");
 var result = document.getElementById("editer");
@@ -13,8 +17,7 @@ element.onchange = function(){
     var fileReader = new FileReader();
 
     fileReader.onload = function(){
-        // console.log(this.result);
-        // resultElement.appendChild(new Text(this.result));
+
         editor.setValue(this.result);
     }
 
